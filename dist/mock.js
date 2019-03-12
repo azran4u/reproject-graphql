31e614ddb39b87519eb7da5633bdd3b53fdbd840
azran4u <azran4u@gmail.com>
2019-03-12 21:33:12 +0200

    removed dist folder

--- dist/mock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mocks = {
-    Int: function () { return 6; },
-    Float: function () { return 22.1; },
-    String: function () { return 'Hello'; },
-};
--- dist/mocks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mocks = {
-    Int: function () { return 6; },
-    Float: function () { return 22.1; },
-    String: function () { return 'Hello'; },
-};
-exports.default = mocks;
-//# sourceMappingURL=mocks.js.map
